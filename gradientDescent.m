146884a38ee7e3dcdabfeb94d4f67cfe9b4f38c5
Andreas Bueff <acbueff@users.noreply.github.com>
2016-04-01 19:28:15 -0700

    Added files via upload
    The first part of the assignment, linear regression

---
+++ gradientDescent.m
@@ -0,0 +1,52 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+%temp0 = 0;
+%temp1 = 0;
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    temp0 = theta(1) - alpha*(partialDerive(X,y,theta,0));
+    temp1 = theta(2) - alpha*(partialDerive(X,y,theta,1)); 
+
+    theta(1) = temp0;
+    theta(2) = temp1;
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
+
+function J = partialDerive(X, y, theta, x)
+        m = length(y);
+        J = 0;
+        for i = 1 : m
+            if x == 1
+                J = J + (hypothesis(X(i,:),theta) - y(i))*X(i,2);
+            else
+                J = J + (hypothesis(X(i,:),theta) - y(i));
+            end
+        end
+
+        J = J * (1/(m));
+end
+
+function htheta = hypothesis(x,theta)
+   htheta = theta' * x';
+end
